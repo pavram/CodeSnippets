c594c5e162677c7aaa4c21bbb599007f99a1bf9b
Peter Avram <peter@avram.com.au>
2016-02-12 10:06:24 +1100

    Barebones string extension methods class.

---
+++ CodeSnippets/StringExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSnippets
+{
+    public static class StringExtensionMethods
+    {
+        /// <summary>
+        /// use a string as a format-string directly.
+        /// Rather than calling the String.Format static function.
+        /// </summary>
+        /// <param name="FormatString"></param>
+        /// <param name="arguments"></param>
+        /// <returns></returns>
+        public static string FormatWith(this string FormatString, params object[] arguments)
+        {
+            return string.Format(FormatString, arguments);
+        }
+    }
+}
